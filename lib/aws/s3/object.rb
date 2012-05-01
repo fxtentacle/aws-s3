b4c7caaa1bf7a36078bece8c70992b529030790a
Hajo Nils Krabbenhöft <fxtentacle@googlemail.com>
2012-05-01 13:35:04 +0200

    escape + character

--- lib/aws/s3/object.rb
+++ lib/aws/s3/object.rb
@@ -297,7 +297,7 @@ module AWS
             options.replace(bucket)
             bucket = nil
           end
-          '/' << File.join(bucket_name(bucket), name)
+          '/' << File.join(bucket_name(bucket), name.gsub(/\+/,'%2B'))
         end
     
         private
